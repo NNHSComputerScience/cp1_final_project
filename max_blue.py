8098adc80bd2b590e06c5b4992e301ac39a495c2
Matt Callaghan <mcallaghan@naperville203.org>
2022-12-14 14:40:21 -0600

    Revised solution to make clearer

--- max_blue.py
+++ max_blue.py
@@ -5,32 +5,54 @@ from PIL import Image
 # WRITE TOGETHER
 def max_blue(image):
 	"""Sets the blue component of the RGB color of every pixel in the picture to the maximum value. An example of manipulating one component of the color of a pixel."""
-	pixels = image.load() # loads the pixel data into an accessible format
+	# load the pixel data into an accessible format
+	pixels = image.load() 
 	width = image.width # get image width in pixels
 	#print(width) # 900px
-	height = image.height
+	height = image.height # get image height in pixels
 	#print(height) # 600px
 	
-	# iterate through each pixel in the image
+	# iterate through (access) each pixel in the image
 	for x in range(width): 
 		for y in range(height):
-			# a pixel is represented by a tuple of RGB values (e.g. (0,255,255))
-			r, g, b = pixels[x, y] # access each pixel and unpack its RGB values
+			# a pixel is represented by a tuple of RGB values 
+			#	e.g. (0,255,0) is a fully green pixel
+			# access each pixel's color tuple
+			color_tuple = pixels[x, y] 
+			# unpack each color_tuple into its RGB values
+			r, g, b = color_tuple
+			
 			if x == 0 and y == 0:
-				print(pixels[x, y]) # see top-left pixel at 0,0
-			image.putpixel((x, y), (r, g, 255)) # Modifies the pixel at the given x,y position.
+				print(r,g,b) # see top-left pixel at 0,0
+				
+			# Modifies the pixel at the given x,y position, replacing it with a new rgb color tuple
+			image.putpixel((x, y), (r, g, 255)) 
 
-	# save the new image
+	# save the new image to the file tree as a PNG file
 	image.save("max_blue.png", "png")
 
 # WRITE ON YOUR OWN - NO COPY/PASTE!
 def max_red(image):
 	"""Sets the red component of the RGB color of every pixel in the picture to the maximum value."""
-	pass
+	pixels = image.load()
+	width = image.width 
+	height = image.height
+	for x in range(width): 
+		for y in range(height):
+			r, g, b = pixels[x, y]
+			image.putpixel((x, y), (255, g, b))
+	image.save("max_red.png", "png") 
 
 def max_green(image):
 	"""Sets the green component of the RGB color of every pixel in the picture to the maximum value."""
-	pass
+	pixels = image.load()
+	width = image.width 
+	height = image.height
+	for x in range(width): 
+		for y in range(height):
+			r, g, b = pixels[x, y]
+			image.putpixel((x, y), (r, 255, b))
+	image.save("max_green.png", "png")  
 
 # MAIN CODE
 
